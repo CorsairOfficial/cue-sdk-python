134a2ba6537258ca405d00d6ee685bc51f3ebc2a
intrueder <intrueder@gmail.com>
2020-04-16 22:13:50 +0300

    Add helpers submodule and ColorRgb class

---
+++ cuesdk/helpers.py
@@ -0,0 +1,48 @@
+__all__ = ['ColorRgb']
+
+
+class ColorRgb(object):
+    def __init__(self, r: int = 0, g: int = 0, b: int = 0):
+        def validate(c):
+            if not 0 <= c <= 255:
+                raise ValueError("The argument must be between 0 and 255.")
+            return int(c)
+
+        self.r = validate(r)
+        self.g = validate(g)
+        self.b = validate(b)
+
+    @property
+    def rgb(self):
+        return (self.r, self.g, self.b)
+
+    @property
+    def hex(self):
+        return "0x%06x" % (((self.r & 0xff) << 16) | ((self.g & 0xff) << 8) |
+                           (self.b & 0xff))
+
+    @staticmethod
+    def _denorm_(fnum):
+        return 255 * max(min(fnum, 1), 0)
+
+    @classmethod
+    def from_vec3(cls, *v3):
+        dn = cls._denorm_
+        col = (dn(c) for c in v3)
+        return cls(*col)
+
+    @classmethod
+    def from_hexstr(cls, hexstr):
+        val = int(hexstr, 16)
+        return cls(r=((val >> 16) & 0xff),
+                   g=((val >> 8) & 0xff),
+                   b=(val & 0xff))
+
+    def __str__(self):
+        return self.hex
+
+    def __repr__(self):
+        return "ColorRgb(%d, %d, %d)" % self.rgb
+
+    def __iter__(self):
+        return iter(self.rgb)
