3253749156b288ba3d5835ece86ab1af3a7d9fdc
Tim-ats-d <tim.arnouts@protonmail.com>
2021-04-11 19:16:10 +0200

    Improve examples.

--- example/events.py
+++ example/events.py
@@ -1,16 +1,20 @@
+
 from cuesdk import CueSdk, CorsairEventId
 
 
-def SdkEventHandler(event_id, data):
-    print("Event: %s" % event_id)
-    if (event_id == CorsairEventId.KeyEvent):
+def sdk_event_handler(event_id, data):
+    print("Event: " + event_id)
+
+    if event_id == CorsairEventId.KeyEvent:
         print(" Device id: %s\n    Key id: %s\n Key state: %s" %
               (data.deviceId.decode(), data.keyId,
-               "pressed" if data.isPressed else "released"))
-    elif (event_id == CorsairEventId.DeviceConnectionStatusChangedEvent):
+               "pressed" if data.isPressed
+               else "released"))
+    elif event_id == CorsairEventId.DeviceConnectionStatusChangedEvent:
         print(" Device id: %s\n    Status: %s" %
               (data.deviceId.decode(),
-               "connected" if data.isConnected else "disconnected"))
+               "connected" if data.isConnected
+               else "disconnected"))
     else:
         print("Invalid event!")
 
@@ -20,22 +24,22 @@ def main():
     connected = sdk.connect()
     if not connected:
         err = sdk.get_last_error()
-        print("Handshake failed: %s" % err)
+        print("Handshake failed: " + err)
         return
 
-    subscribed = sdk.subscribe_for_events(SdkEventHandler)
+    subscribed = sdk.subscribe_for_events(sdk_event_handler)
     if not subscribed:
         err = sdk.get_last_error()
-        print("Subscribe for events error: %s" % err)
+        print("Subscribe for events error: " + err)
         return
 
-    print("Working... Press any G/M key or connect/disconnect Corsair device" +
-          " to see events in action\n")
-    print("Press \"q\" to close program...")
+    print("Working... Press any G/M key or connect/disconnect Corsair device"
+           " to see events in action\n")
+    print('Press "q" to close program...')
 
-    while (True):
+    while True:
         input_str = input()
-        if input_str == "q" or input_str == "Q":
+        if input_str.lower() == "q":
             print("Exiting.")
             break
 
