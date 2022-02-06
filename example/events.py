297be6d73d72696acfcade3407c7e92d4b43e8a0
Tymur Mudzhyri <intrueder@gmail.com>
2022-02-06 20:56:44 +0200

    Fix messages output in events.py example

--- example/events.py
+++ example/events.py
@@ -1,20 +1,16 @@
-
 from cuesdk import CueSdk, CorsairEventId
 
 
 def sdk_event_handler(event_id, data):
-    print("Event: " + event_id)
-
-    if event_id == CorsairEventId.KeyEvent:
+    print("Event: %s" % event_id)
+    if (event_id == CorsairEventId.KeyEvent):
         print(" Device id: %s\n    Key id: %s\n Key state: %s" %
               (data.deviceId.decode(), data.keyId,
-               "pressed" if data.isPressed
-               else "released"))
-    elif event_id == CorsairEventId.DeviceConnectionStatusChangedEvent:
+               "pressed" if data.isPressed else "released"))
+    elif (event_id == CorsairEventId.DeviceConnectionStatusChangedEvent):
         print(" Device id: %s\n    Status: %s" %
               (data.deviceId.decode(),
-               "connected" if data.isConnected
-               else "disconnected"))
+               "connected" if data.isConnected else "disconnected"))
     else:
         print("Invalid event!")
 
@@ -24,18 +20,18 @@ def main():
     connected = sdk.connect()
     if not connected:
         err = sdk.get_last_error()
-        print("Handshake failed: " + err)
+        print("Handshake failed: %s" % err)
         return
 
     subscribed = sdk.subscribe_for_events(sdk_event_handler)
     if not subscribed:
         err = sdk.get_last_error()
-        print("Subscribe for events error: " + err)
+        print("Subscribe for events error: %s" % err)
         return
 
     print("Working... Press any G/M key or connect/disconnect Corsair device"
-           " to see events in action\n")
-    print('Press "q" to close program...')
+          " to see events in action\n")
+    print("Press 'q' to close program...")
 
     while True:
         input_str = input()
